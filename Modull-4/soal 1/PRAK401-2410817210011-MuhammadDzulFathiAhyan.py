9991c6a195f8a62761503f59103371c83394c607
ZulFath1 <2410817210011@mhs.ulm.ac.id>
2024-12-14 14:07:55 +0800

    Delete Modull-4 directory

--- Modull-4/soal 1/PRAK401-2410817210011-MuhammadDzulFathiAhyan.py
+++
@@ -1,7 +0,0 @@
-r, a = input().split()
-r =int(r)
-for n in range (1, 51):
-    if n % r == 0:
-        print (a, end=' ')
-    else :
-        print (n, end=' ')
--- Modull-4/soal 2/PRAK402-2410817210011-MuhammadDzulFathiAhyan.py
+++
@@ -1,9 +0,0 @@
-r=int(input())
-for i in range (1,r):
-    if i % 2 != 0 :
-        print(i,end=" ")
-print("")
-while r>0 :
-    if r %2 == 0 :
-        print (r,end=" ")
-    r -= 1  
